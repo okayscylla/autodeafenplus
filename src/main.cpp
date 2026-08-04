a44ea0c6584318835d9477eb3473f903816c73e8
okayscylla <75046957+okayscylla@users.noreply.github.com>
2026-08-04 18:29:46 -0400

    i hate libzmq

--- src/main.cpp
+++ src/main.cpp
@@ -236,9 +236,9 @@ int user_platform;
 bool active = false;
 
 
-void *b_context = zmq_ctx_new();
+// void *b_context = zmq_ctx_new();
 
-void *b_socket = zmq_socket(b_context, ZMQ_PUSH);
+// void *b_socket = zmq_socket(b_context, ZMQ_PUSH);
 
 
 $on_game(Loaded) {
